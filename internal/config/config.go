de884ad88c70e9f213716dff74e4bea568503cca
Andrew Polshkov <andrey.alpo@yandex.ru>
2022-06-21 22:25:47 +0500

    increment7: добавлена возможность конфигруации с помощью флагов командной строки

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"github.com/caarlos0/env"
 )
 
@@ -14,6 +15,26 @@ func Load() Config {
 	var cfg Config
 	if err := env.Parse(&cfg); err != nil {
 		panic(err)
+	}
+
+	var addressFlagValue string
+	var baseURLFlagValue string
+	var fileStoragePathFlagValue string
+
+	flag.StringVar(&addressFlagValue, "a", "", "HTTP server start address")
+	flag.StringVar(&baseURLFlagValue, "b", "", "base address of the resulting shortened URL")
+	flag.StringVar(&fileStoragePathFlagValue, "f", "", "path to file with abbreviated URLs")
+
+	flag.Parse()
+
+	if addressFlagValue != "" {
+		cfg.ServerAddress = addressFlagValue
+	}
+	if baseURLFlagValue != "" {
+		cfg.BaseURL = baseURLFlagValue
+	}
+	if fileStoragePathFlagValue != "" {
+		cfg.FileStoragePath = fileStoragePathFlagValue
 	}
 
 	return cfg
