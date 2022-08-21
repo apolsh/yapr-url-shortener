f3da10a6245d8b430ea0f49d53959e383688567f
Polshkov Andrey <andrey.alpo@yandex.ru>
2022-08-21 18:08:17 +0500

    increment10: убрал дефолтное значение для DATABASE_DSN

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -10,7 +10,8 @@ type Config struct {
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 	AuthSecretKey   string `env:"AUTH_SECRET_KEY" envDefault:"very_secret_key"`
-	DatabaseDSN     string `env:"DATABASE_DSN" envDefault:"postgresql://yaprurlshortener:yaPR_ttuss@localhost:5432/yapr-url-shortener"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
+	//DatabaseDSN     string `env:"DATABASE_DSN" envDefault:"postgresql://yaprurlshortener:yaPR_ttuss@localhost:5432/yapr-url-shortener"`
 }
 
 func Load() Config {
