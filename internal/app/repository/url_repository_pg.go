e1ebe88f39bf807eb42e02776efce7c5afed6561
Polshkov Andrey <andrey.alpo@yandex.ru>
2022-08-22 02:30:48 +0500

    increment13: уникальность OriginalURL в таблице

--- internal/app/repository/url_repository_pg.go
+++ internal/app/repository/url_repository_pg.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"log"
 	"time"
 )
 
@@ -155,7 +156,7 @@ func setupTable(conn *pgxpool.Pool) error {
 						owner        uuid        not null
 					)`
 	createIDIndexQ := "create unique index if not exists shortened_urls_id_uindex on shortened_urls (id)"
-	//createOriginalURLIndexQ := "create unique index shortened_urls_original_url_uindex on shortened_urls (original_url)"
+	createOriginalURLIndexQ := "create unique index shortened_urls_original_url_uindex on shortened_urls (original_url)"
 	_, err = tx.Exec(ctx, createTableQ)
 	if err != nil {
 		return err
@@ -164,10 +165,13 @@ func setupTable(conn *pgxpool.Pool) error {
 	if err != nil {
 		return err
 	}
-	//_, err = tx.Exec(ctx, createOriginalURLIndexQ)
-	//if err != nil {
-	//	return err
-	//}
+	log.Println("execute createOriginalURLIndexQ")
+	_, err = tx.Exec(ctx, createOriginalURLIndexQ)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	log.Println("done with createOriginalURLIndexQ")
 	_, err = tx.Prepare(ctx, preparedSaveBatchStatement, "INSERT INTO shortened_urls (id, original_url, owner) VALUES ($1, $2, $3)")
 	if err != nil {
 		return err
