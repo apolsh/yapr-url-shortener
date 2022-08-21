0778b130d25f8b7b4c0b30b81b86f24c511f7f6e
Polshkov Andrey <andrey.alpo@yandex.ru>
2022-08-22 03:58:32 +0500

    increment13: рефактор

--- internal/app/middleware/auth_middleware.go
+++ internal/app/middleware/auth_middleware.go
@@ -10,7 +10,7 @@ import (
 
 const authCookieName = "sessionId"
 
-type ContextKey = string
+type ContextKey string
 
 var OwnerID ContextKey = "userId"
 
@@ -52,73 +52,4 @@ func AuthMiddleware(cryptoProvider crypto.Provider) func(http.Handler) http.Hand
 		}
 		return http.HandlerFunc(handlerFunction)
 	}
-
-	//return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//	cookie, err := r.Cookie(authCookieName)
-	//	if err != nil {
-	//		if errors.Is(err, http.ErrNoCookie) {
-	//			userID := uuid.New()
-	//			userUUID := sha256.Sum256(userID[:])
-	//
-	//			src := []byte("Этюд в розовых тонах") // данные, которые хотим зашифровать
-	//
-	//			// будем использовать AES256, создав ключ длиной 32 байта
-	//			key, err := generateRandom(2 * aes.BlockSize) // ключ шифрования
-	//			if err != nil {
-	//				fmt.Printf("error: %v\n", err)
-	//				return
-	//			}
-	//
-	//			aesblock, err := aes.NewCipher(key)
-	//			if err != nil {
-	//				fmt.Printf("error: %v\n", err)
-	//				return
-	//			}
-	//
-	//			aesgcm, err := cipher.NewGCM(aesblock)
-	//			if err != nil {
-	//				fmt.Printf("error: %v\n", err)
-	//				return
-	//			}
-	//
-	//			// создаём вектор инициализации
-	//			nonce, err := generateRandom(aesgcm.NonceSize())
-	//			if err != nil {
-	//				fmt.Printf("error: %v\n", err)
-	//				return
-	//			}
-	//
-	//			dst := aesgcm.Seal(nil, nonce, src, nil) // зашифровываем
-	//			fmt.Printf("encrypted: %x\n", dst)
-	//
-	//			src2, err := aesgcm.Open(nil, nonce, dst, nil) // расшифровываем
-	//			if err != nil {
-	//				fmt.Printf("error: %v\n", err)
-	//				return
-	//			}
-	//			fmt.Printf("decrypted: %s\n", src2)
-	//
-	//		}
-	//	}
-	//
-	//	//if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-	//	//	next.ServeHTTP(w, r)
-	//	//	return
-	//	//}
-	//	//
-	//	//gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-	//	//if err != nil {
-	//	//	http.Error(w, "Error while body reading", http.StatusInternalServerError)
-	//	//}
-	//	//defer gz.Close()
-	//	//
-	//	//w.Header().Set("Content-Encoding", "gzip")
-	//	sessionIdCookie := &http.Cookie{Name: "sessionId", Value: "abc"}
-	//	http.SetCookie(w, sessionIdCookie)
-	//
-	//	ctxWithUser := context.WithValue(r.Context(), OwnerID, "user")
-	//	rWithUser := r.WithContext(ctxWithUser)
-	//	next.ServeHTTP(w, rWithUser)
-	//}
-
 }
