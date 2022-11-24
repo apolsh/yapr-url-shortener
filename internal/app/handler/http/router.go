95888f6d6ad64d6e92f8e667b0ee073bd165978b
Polshkov Andrey <andrey.alpo@yandex.ru>
2022-11-25 01:04:07 +0500

    increment17: добавлены примеры работы с эндпоинтами

--- internal/app/handler/http/router.go
+++ internal/app/handler/http/router.go
@@ -33,12 +33,12 @@ const (
 	applicationJSON = "application/json; charset=utf-8"
 )
 
-type controller struct {
+type Controller struct {
 	shortenService service.URLShortenerService
 }
 
 func NewRouter(r *chi.Mux, serviceImpl service.URLShortenerService, provider crypto.CryptographicProvider) {
-	c := &controller{shortenService: serviceImpl}
+	c := &Controller{shortenService: serviceImpl}
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
@@ -66,7 +66,7 @@ func NewRouter(r *chi.Mux, serviceImpl service.URLShortenerService, provider cry
 }
 
 // PingDB проверяет работу хранилища URL
-func (c *controller) PingDB(w http.ResponseWriter, _ *http.Request) {
+func (c *Controller) PingDB(w http.ResponseWriter, _ *http.Request) {
 	ok := c.shortenService.PingDB()
 	if ok {
 		w.WriteHeader(200)
@@ -76,7 +76,7 @@ func (c *controller) PingDB(w http.ResponseWriter, _ *http.Request) {
 }
 
 // GetShortenURLByID производит редирект на сохраненный ранее в хранилище URL
-func (c *controller) GetShortenURLByID(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) GetShortenURLByID(w http.ResponseWriter, r *http.Request) {
 	if urlID := chi.URLParam(r, "urlID"); urlID != "" {
 		foundURL, err := c.shortenService.GetURLByID(urlID)
 		if errors.Is(repository.ErrorItemNotFound, err) {
@@ -94,7 +94,7 @@ func (c *controller) GetShortenURLByID(w http.ResponseWriter, r *http.Request) {
 }
 
 // GetShortenURLsByUser возвращает список пар (короткий + длинный) URL пользователя
-func (c *controller) GetShortenURLsByUser(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) GetShortenURLsByUser(w http.ResponseWriter, r *http.Request) {
 	ownerID := r.Context().Value(customMiddleware.OwnerID).(string)
 	urlPairs, err := c.shortenService.GetURLsByOwnerID(ownerID)
 	if err != nil {
@@ -114,7 +114,7 @@ func (c *controller) GetShortenURLsByUser(w http.ResponseWriter, r *http.Request
 }
 
 // SaveShortenURL принимает запрос в виде простого текста, сохраняет URL в хранилище
-func (c *controller) SaveShortenURL(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) SaveShortenURL(w http.ResponseWriter, r *http.Request) {
 	if !isValidContentType(r, "text/plain", "text", "application/x-gzip") {
 		http.Error(w, invalidContentTypeError, http.StatusBadRequest)
 		return
@@ -155,7 +155,7 @@ func (c *controller) SaveShortenURL(w http.ResponseWriter, r *http.Request) {
 }
 
 // SaveShortenURLsInBatch сохраняет сразу несколько URL в хранилище за один запрос
-func (c *controller) SaveShortenURLsInBatch(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) SaveShortenURLsInBatch(w http.ResponseWriter, r *http.Request) {
 	var body []dto.ShortenInBatchRequestItem
 
 	err := extractJSONBody(r, &body)
@@ -179,7 +179,7 @@ func (c *controller) SaveShortenURLsInBatch(w http.ResponseWriter, r *http.Reque
 }
 
 // SaveShortenURLJSON принимает запрос в виде JSON, сохраняет URL в хранилище
-func (c *controller) SaveShortenURLJSON(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) SaveShortenURLJSON(w http.ResponseWriter, r *http.Request) {
 	var body SaveURLBody
 	err := extractJSONBody(r, &body)
 	if err != nil {
@@ -216,7 +216,7 @@ func (c *controller) SaveShortenURLJSON(w http.ResponseWriter, r *http.Request) 
 }
 
 // DeleteShortenURLsInBatch помечает URL в хранилище как удаленный
-func (c *controller) DeleteShortenURLsInBatch(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) DeleteShortenURLsInBatch(w http.ResponseWriter, r *http.Request) {
 	var ids []string
 	err := extractJSONBody(r, &ids)
 	if err != nil {
