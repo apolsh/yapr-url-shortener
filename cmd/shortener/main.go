3a5d9c9f75b7a6ee3306a35fff3eb459b78302fe
Polshkov Andrey <andrey.alpo@yandex.ru>
2022-12-17 18:54:27 +0500

    increment19: добавил для теста gracefull shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/apolsh/yapr-url-shortener/internal/app/config"
 	"github.com/apolsh/yapr-url-shortener/internal/app/crypto"
@@ -52,5 +56,40 @@ func main() {
 
 	router.Mount("/debug", middleware.Profiler())
 
-	log.Fatal(http.ListenAndServe(cfg.ServerAddress, router))
+	done := make(chan bool)
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, os.Interrupt)
+
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	server := &http.Server{
+		Addr:         cfg.ServerAddress,
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	go func() {
+		<-quit
+		log.Println("server is shutting down...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		close(done)
+	}()
+
+	logger.Println("Server is ready to handle requests at", cfg.ServerAddress)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Could not listen on %s: %v\n", cfg.ServerAddress, err)
+	}
+
+	<-done
+	logger.Println("Server stopped")
 }
